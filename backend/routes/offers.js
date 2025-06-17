0a2a4ce8d9d834ca652640b704fdca394e61bd7a
metrike <kevinmetri.pro@gmail.com>
2025-06-17 16:21:55 +0200

    setup

--- backend/routes/offers.js
+++ backend/routes/offers.js
@@ -43,7 +43,7 @@ router.get('/', async (req, res) => {
 });
 
 // ✅ Route par ID avec Neo4j
-router.get('/:id([0-9a-fA-F]{24})', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const offerId = req.params.id;
     const redisKey = `offers:${offerId}`;
 
@@ -57,6 +57,7 @@ router.get('/:id([0-9a-fA-F]{24})', async (req, res) => {
 
         const Offer = mongoose.model('Offer');
         const offer = await Offer.findById(offerId).lean();
+        Offer.find().then(console.log)
         if (!offer) return res.status(404).json({ error: 'Offer not found' });
 
         const session = neo4jDriver.session();
--- backend/services/neo4j.js
+++ backend/services/neo4j.js
@@ -1,8 +1,8 @@
 import neo4j from 'neo4j-driver';
 
 const driver = neo4j.driver(
-    'bolt://localhost:7687',
-    neo4j.auth.basic('neo4j', 'test') // ou ton mot de passe défini dans docker-compose
+    'bolt://neo4j:7687', // ✔️ le nom du service docker-compose
+    neo4j.auth.basic('neo4j', 'supersecure123') // ou ton mot de passe défini dans docker-compose
 );
 
 export default driver;
